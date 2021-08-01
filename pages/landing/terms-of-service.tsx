abc9e28751725bab73a1b026095a056434fc0033
ThahaShahzad <61521512+ThahaShahzad@users.noreply.github.com>
2021-08-01 09:47:48 -0500

    build fix 4

--- pages/landing/terms-of-service.tsx
+++ pages/landing/terms-of-service.tsx
@@ -120,8 +120,8 @@ const TermsofService = () => {
           <p>
             Notwithstanding any damages that You might incur, the entire liability of the Company and any of its
             suppliers under any provision of this Terms and Your exclusive remedy for all of the foregoing shall be
-            limited to the amount actually paid by You through the Service or 100 USD if You haven't purchased anything
-            through the Service.
+            limited to the amount actually paid by You through the Service or 100 USD if You haven&apos;t purchased
+            anything through the Service.
           </p>
           <p>
             To the maximum extent permitted by applicable law, in no event shall the Company or its suppliers be liable
@@ -135,7 +135,7 @@ const TermsofService = () => {
           <p>
             Some states do not allow the exclusion of implied warranties or limitation of liability for incidental or
             consequential damages, which means that some of the above limitations may not apply. In these states, each
-            party's liability will be limited to the greatest extent permitted by law.
+            party&apos;s liability will be limited to the greatest extent permitted by law.
           </p>
 
           <h1>&quot;AS IS&quot; and &quot;AS AVAILABLE&quot; Disclaimer</h1>
@@ -153,7 +153,7 @@ const TermsofService = () => {
             can or will be corrected.
           </p>
           <p>
-            Without limiting the foregoing, neither the Company nor any of the company's provider makes any
+            Without limiting the foregoing, neither the Company nor any of the company&apos;s provider makes any
             representation or warranty of any kind, express or implied: (i) as to the operation or availability of the
             Service, or the information, content, and materials or products included thereon; (ii) that the Service will
             be uninterrupted or error-free; (iii) as to the accuracy, reliability, or currency of any information or
@@ -206,8 +206,8 @@ const TermsofService = () => {
           <h2>Waiver</h2>
           <p>
             Except as provided herein, the failure to exercise a right or to require performance of an obligation under
-            this Terms shall not effect a party's ability to exercise such right or require such performance at any time
-            thereafter nor shall be the waiver of a breach constitute a waiver of any subsequent breach.
+            this Terms shall not effect a party&apos;s ability to exercise such right or require such performance at any
+            time thereafter nor shall be the waiver of a breach constitute a waiver of any subsequent breach.
           </p>
 
           <h1>Translation Interpretation</h1>
@@ -219,8 +219,8 @@ const TermsofService = () => {
           <h1>Changes to These Terms and Conditions</h1>
           <p>
             We reserve the right, at Our sole discretion, to modify or replace these Terms at any time. If a revision is
-            material We will make reasonable efforts to provide at least 30 days' notice prior to any new terms taking
-            effect. What constitutes a material change will be determined at Our sole discretion.
+            material We will make reasonable efforts to provide at least 30 days&apos; notice prior to any new terms
+            taking effect. What constitutes a material change will be determined at Our sole discretion.
           </p>
           <p>
             By continuing to access or use Our Service after those revisions become effective, You agree to be bound by
