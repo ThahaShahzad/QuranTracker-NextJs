ce409457200051b462773fa5037aeeea37506365
ThahaShahzad <61521512+ThahaShahzad@users.noreply.github.com>
2021-08-01 23:42:12 -0500

    Firebase Cli
    Added
    Firebase Auth Hosting Firestore db storage

--- next.config.js
+++ next.config.js
@@ -1,11 +1,7 @@
 module.exports = {
   reactStrictMode: true,
   target: 'experimental-serverless-trace',
-  env: {
-    NETLIFY_CLIENT_ID: 'zstwJxxk5haiImE5QXRzzYirIWYk4Kctu3z1eebvA0w',
-    NETLIFY_CLIENT_SECRET: '8nMS89AypMAS_LaJaEq22uVq7DWqBndZj1tFXEuIo3I',
-    DATABASE_URL: '',
-    mongodburl:
-      'mongodb+srv://TS-Admind:*****@polestartest.p85rh.mongodb.net/test?authSource=admin&replicaSet=atlas-k21te9-shard-0&readPreference=primary&appname=MongoDB%20Compass&ssl=true'
+  images: {
+    domains: ['images.unsplash.com']
   }
 }
